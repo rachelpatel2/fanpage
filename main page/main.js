2a9644eac55ebcf721090f159d3194caf1652090
Rachel Patel <rpatel2@colum.edu>
2020-11-24 13:58:54 -0600

    button

--- main page/main.js
+++ main page/main.js
@@ -1,24 +1,18 @@
-var modal = document.getElementById("myModal");
+var modal = document.querySelector(".modal");
+    var trigger = document.querySelector(".trigger");
+    var closeButton = document.querySelector(".close-button");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+    function toggleModal() {
+        modal.classList.toggle("show-modal");
+    }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    function windowOnClick(event) {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    }
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
+	
